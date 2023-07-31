9ad9677b2310ec286fdc5d974a7388023eb3e8f0
cgarcia <garcia-maurino.extern@univention.de>
2023-07-31 07:34:28 +0200

    Issue univention/internal/research-library#52: return dict instead of orjson reponse

--- management-api/guardian_management_api/routes.py
+++ management-api/guardian_management_api/routes.py
@@ -2,13 +2,11 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 
-from typing import Annotated
+from typing import Annotated, Any, Dict
 
 from fastapi import APIRouter, Depends
-from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import HTTPException
 from fastapi.params import Body
-from fastapi.responses import ORJSONResponse
 
 from . import business_logic
 from .adapter_registry import port_dep
@@ -31,31 +29,28 @@ async def create_app(
         port_dep(AppAPIPort, FastAPIAppAPIAdapter)
     ),
     persistence: AppPersistencePort = Depends(port_dep(AppPersistencePort)),
-):
-    return ORJSONResponse(
-        content=jsonable_encoder(
-            await business_logic.create_app(
-                api_request=app_create_request,
-                management_app_api_port=management_app_api,
-                persistence_port=persistence,
-            )
-        )
+) -> Dict[str, Any]:
+    response: ManagementAppCreateResponse = await business_logic.create_app(
+        api_request=app_create_request,
+        management_app_api_port=management_app_api,
+        persistence_port=persistence,
     )
+    return response.dict()
 
 
-@router.get("/apps/{name}", response_model=ManagementAppGetResponse | None)
+@router.get("/apps/{name}", response_model=ManagementAppGetResponse)
 async def get_app(
     app_get_request: ManagementAppGetRequest = Depends(),
     management_app_api: FastAPIAppAPIAdapter = Depends(
         port_dep(AppAPIPort, FastAPIAppAPIAdapter)
     ),
     persistence: AppPersistencePort = Depends(port_dep(AppPersistencePort)),
-):
-    app: ManagementAppGetResponse | None = await business_logic.get_app(
+) -> Dict[str, Any]:
+    response: ManagementAppGetResponse | None = await business_logic.get_app(
         api_request=app_get_request,
         management_app_api_port=management_app_api,
         persistence_port=persistence,
     )
-    if app is None:
+    if response is None:
         raise HTTPException(status_code=404, detail="App not found")
-    return ORJSONResponse(content=jsonable_encoder(app))
+    return response.dict()
