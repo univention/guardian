62c96d4b20b82237da4a00a45e962bea7f964c42
cgarcia <garcia-maurino.extern@univention.de>
2023-07-31 07:52:22 +0200

    Issue univention/internal/research-library#52: add docstrings to errors

--- management-api/guardian_management_api/errors.py
+++ management-api/guardian_management_api/errors.py
@@ -4,36 +4,51 @@
 
 
 class SettingNotFoundError(ValueError):
+    """If no value for the specified setting can be found.
+
+    And no default is specified.
+    """
+
     ...
 
 
 class SettingTypeError(TypeError):
+    """If a value was found, but cannot be converted to the expected type."""
+
     ...
 
 
 class SettingFormatError(RuntimeError):
+    """If the requested setting name is malformed."""
+
     ...
 
 
 class ObjectNotFoundError(ValueError):
+    """If the requested object could not be found."""
+
     ...
 
 
 class PersistenceError(RuntimeError):
+    """For any errors other than object not found."""
+
     ...
 
 
 class AdapterLoadingError(RuntimeError):
+    """For errors that occure during loading of an adapter."""
+
     ...
 
 
 class AdapterInitializationError(RuntimeError):
+    """For errors that occure during initialization of an adapter."""
+
     ...
 
 
 class AdapterConfigurationError(ValueError):
-    ...
+    """For errors that occure during configuration of an adapter."""
 
-
-class PolicyUpstreamError(Exception):
     ...
