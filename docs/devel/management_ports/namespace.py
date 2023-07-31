dc3a2a8139f21f38948a6afcd87d35138fa373bf
cgarcia <garcia-maurino.extern@univention.de>
2023-07-31 08:12:39 +0200

    Revert "Issue univention/internal/research-library#52: remove ports"
    This reverts commit f22d97097bd0b453db22ece8f200f10ec55f9fd5.

---
+++ docs/devel/management_ports/namespace.py
@@ -0,0 +1,125 @@
+# Copyright (C) 2023 Univention GmbH
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+"""
+Proposed layout for the namespace ports/models
+"""
+
+from dataclasses import dataclass
+from typing import List, Optional
+
+from .base import BasePort, PaginatedAPIResponse, QueryResponse, ResponseObject
+
+###############################################################################
+#                                                                             #
+#  Models                                                                     #
+#                                                                             #
+###############################################################################
+
+
+@dataclass
+class Namespace:
+    app_name: str
+    name: str
+    display_name: Optional[str]
+
+
+class NamespaceQuery(QueryResponse):
+    namespaces: List[Namespace]
+
+
+# We're intentionally not tying this to Namespace,
+# because we'd like to make sure changes to the persistence layer don't
+# accidentally affect the response layer.
+class ResponseNamespace(ResponseObject):
+    app_name: str
+    name: str
+    display_name: Optional[str]
+
+
+@dataclass
+class NamespaceAPIResponse:
+    namespace: ResponseNamespace
+
+
+class NamespacesListAPIResponse(PaginatedAPIResponse):
+    namespaces: List[ResponseNamespace]
+
+
+###############################################################################
+#                                                                             #
+#  Incoming ports                                                             #
+#                                                                             #
+###############################################################################
+
+
+class NamespaceAPIPort(BasePort):
+    async def create(
+        self,
+        app_name: str,
+        namespace_name: str,
+        display_name: Optional[str] = None,
+    ) -> NamespaceAPIResponse:
+        pass
+
+    async def read_one(
+        self,
+        app_name: str,
+        namespace_name: str,
+    ) -> NamespaceAPIResponse:
+        pass
+
+    async def read_many(
+        self,
+        app_name: Optional[str] = None,
+        namespace_name: Optional[str] = None,
+        pagination_offset: Optional[int] = None,
+        pagination_limit: Optional[int] = None,
+    ) -> NamespacesListAPIResponse:
+        pass
+
+    async def update(
+        self,
+        app_name: str,
+        namespace_name: str,
+        display_name: str,
+    ) -> NamespaceAPIResponse:
+        pass
+
+
+###############################################################################
+#                                                                             #
+#  Outgoing ports                                                             #
+#                                                                             #
+###############################################################################
+
+
+class NamespacePersistencePort(BasePort):
+    async def create(
+        self,
+        namespace: Namespace,
+    ) -> Namespace:
+        pass
+
+    async def read_one(
+        self,
+        app_name: str,
+        namespace_name: str,
+    ) -> Namespace:
+        pass
+
+    async def read_many(
+        self,
+        app_name: Optional[str] = None,
+        namespace_name: Optional[str] = None,
+        query_offset: Optional[int] = None,
+        query_limit: Optional[int] = None,
+    ) -> NamespaceQuery:
+        pass
+
+    async def update(
+        self,
+        namespace: Namespace,
+    ) -> Namespace:
+        pass
---
+++ docs/devel/management_ports/permission.py
@@ -0,0 +1,127 @@
+# Copyright (C) 2023 Univention GmbH
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+"""
+Proposed layout for permission ports/models
+"""
+
+from dataclasses import dataclass
+from typing import List, Optional
+
+from .base import (
+    BasePort,
+    NamespacedPersistenceObject,
+    NamespacedResponseObject,
+    PaginatedAPIResponse,
+    QueryResponse,
+)
+
+###############################################################################
+#                                                                             #
+#  Models                                                                     #
+#                                                                             #
+###############################################################################
+
+
+class Permission(NamespacedPersistenceObject):
+    ...
+
+
+class PermissionQuery(QueryResponse):
+    permissions: List[Permission]
+
+
+class ResponsePermission(NamespacedResponseObject):
+    ...
+
+
+@dataclass
+class PermissionAPIResponse:
+    permission: ResponsePermission
+
+
+class PermissionsListAPIResponse(PaginatedAPIResponse):
+    permissions: List[ResponsePermission]
+
+
+###############################################################################
+#                                                                             #
+#  Incoming ports                                                             #
+#                                                                             #
+###############################################################################
+
+
+class PermissionAPIPort(BasePort):
+    def create(
+        self,
+        app_name: str,
+        namespace_name: str,
+        permission_name: str,
+        display_name: Optional[str] = None,
+    ) -> PermissionAPIResponse:
+        pass
+
+    def read_one(
+        self,
+        app_name: str,
+        namespace_name: str,
+        permission_name: str,
+    ) -> PermissionAPIResponse:
+        pass
+
+    def read_many(
+        self,
+        app_name: Optional[str] = None,
+        namespace_name: Optional[str] = None,
+        pagination_offset: Optional[int] = None,
+        pagination_limit: Optional[int] = None,
+    ) -> PermissionsListAPIResponse:
+        pass
+
+    def update(
+        self,
+        app_name: str,
+        namespace_name: str,
+        permission_name: str,
+        display_name: str,
+    ) -> PermissionAPIResponse:
+        pass
+
+
+###############################################################################
+#                                                                             #
+#  Outgoing ports                                                             #
+#                                                                             #
+###############################################################################
+
+
+class PermissionPersistencePort(BasePort):
+    async def create(
+        self,
+        permission: Permission,
+    ) -> Permission:
+        pass
+
+    async def read_one(
+        self,
+        app_name: str,
+        namespace_name: str,
+        permission_name: str,
+    ) -> Permission:
+        pass
+
+    async def read_many(
+        self,
+        app_name: Optional[str] = None,
+        namespace_name: Optional[str] = None,
+        query_offset: Optional[int] = None,
+        query_limit: Optional[int] = None,
+    ) -> PermissionQuery:
+        pass
+
+    async def update(
+        self,
+        permission: Permission,
+    ) -> Permission:
+        pass
