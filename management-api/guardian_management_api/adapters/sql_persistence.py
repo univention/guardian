b32fca06d2a9a6b544841acc62ed1000af03b87b
Ole Schwiegert <schwiegert@univention.de>
2023-08-30 14:07:31 +0200

    univention/components/authorization-engine/guardian#33: Add sqlalchemy, alembic, SQLAlchemyMixin and first migration

---
+++ management-api/guardian_management_api/adapters/sql_persistence.py
@@ -0,0 +1,175 @@
+import sqlite3
+from functools import wraps
+from typing import Optional, Type, TypeVar
+
+from sqlalchemy import Engine, event, select
+from sqlalchemy.dialects.sqlite.aiosqlite import AsyncAdapt_aiosqlite_connection
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.sql.functions import count
+
+from ..errors import ObjectExistsError, ParentNotFoundError, PersistenceError
+
+
+def error_guard(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        try:
+            return await func(*args, **kwargs)
+        except SQLAlchemyError as exc:
+            raise PersistenceError("An unidentified error occurred.") from exc
+
+    return wrapper
+
+
+ORMObj = TypeVar("ORMObj")
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    """
+    This event listener activates the foreign key constraint evaluation on sqlite databases
+
+    https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#foreign-key-support
+    """
+    if isinstance(dbapi_connection, AsyncAdapt_aiosqlite_connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
+
+
+class SQLAlchemyMixin:
+    def __init__(self):
+        self._sql_engine = None
+        self._session = None
+        self._db_string = ""
+        self._dialect = ""
+
+    @staticmethod
+    def create_db_string(
+        dialect: str, host: str, port: str, db_name: str, username: str, password: str
+    ) -> str:
+        if dialect == "sqlite":
+            return (
+                f"sqlite+aiosqlite:///{db_name}" if db_name else "sqlite+aiosqlite://"
+            )
+        elif True:  # Remove with univention/components/authorization-engine/guardian#98
+            raise ValueError(
+                f"The dialect {dialect} is not supported. "
+                f"Support for mysql and postgresql will be added soon!"
+            )
+        elif dialect == "postgresql":
+            if not (host and db_name and username and password):
+                raise ValueError(
+                    "The dialect postgresql requires a host, db_name, username and password to connect."
+                )
+            dialect = "postgresql+asyncpg"
+        elif dialect == "mysql":
+            if not (host and db_name and username and password):
+                raise ValueError(
+                    "The dialect mysql requires a host, db_name, username and password to connect."
+                )
+            dialect = "mysql+aiomysql"
+        else:
+            raise ValueError(f"The dialect {dialect} is not supported.")
+        credentials = f"{username}:{password}@" if username and password else ""
+        port = f":{port}" if port else ""
+        db_name = f"/{db_name}" if db_name else ""
+        return f"{dialect}://{credentials}{host}{port}{db_name}"
+
+    @staticmethod
+    def session_wrapper(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            if kwargs.get("session") is None:
+                async with args[0].session() as session:
+                    kwargs["session"] = session
+                    return await func(*args, **kwargs)
+            else:
+                return await func(*args, **kwargs)
+
+        return wrapper
+
+    @property
+    def sql_engine(self) -> AsyncEngine:  # pragma: no cover
+        if self._sql_engine is None:
+            self._sql_engine = create_async_engine(self._db_string)
+        return self._sql_engine
+
+    @property
+    def session(self):  # pragma: no cover
+        if self._session is None:
+            self._session = async_sessionmaker(self.sql_engine, expire_on_commit=False)
+        return self._session
+
+    @error_guard
+    async def _get_single_object(
+        self, orm_cls: Type[ORMObj], **identifiers
+    ) -> Optional[ORMObj]:
+        stmt = select(orm_cls).where(
+            *[getattr(orm_cls, attr) == value for attr, value in identifiers.items()]
+        )
+        async with self.session() as session:
+            return (await session.execute(stmt)).scalar()
+
+    @error_guard
+    async def _get_num_objects(self, orm_cls: Type[ORMObj]):
+        async with self.session() as session:
+            return await session.scalar(select(count(getattr(orm_cls, "id"))))
+
+    @error_guard
+    async def _get_many_objects(
+        self,
+        orm_cls: Type[ORMObj],
+        offset: int,
+        limit: Optional[int],
+        order_by: str = "name",
+    ) -> list[ORMObj]:
+        async with self.session() as session:
+            stmt = select(orm_cls).offset(offset).order_by(getattr(orm_cls, order_by))
+            if limit:
+                stmt = stmt.limit(limit)
+            return (await session.scalars(stmt)).all()
+
+    @error_guard
+    @session_wrapper
+    async def _create_object(self, obj: ORMObj, session: AsyncSession) -> ORMObj:
+        try:
+            async with session.begin():
+                session.add(obj)
+        except (
+            IntegrityError
+        ) as exc:  # This needs separate handling for each underlying dialect.
+            # See univention/components/authorization-engine/guardian#98
+            # print(exc.orig.__dict__)
+            if (
+                isinstance(exc.orig, sqlite3.IntegrityError)
+                and exc.orig.sqlite_errorname == "SQLITE_CONSTRAINT_UNIQUE"
+            ):
+                raise ObjectExistsError(
+                    "An object with the given identifiers already exists."
+                )
+            elif (
+                isinstance(exc.orig, sqlite3.IntegrityError)
+                and exc.orig.sqlite_errorname == "SQLITE_CONSTRAINT_FOREIGNKEY"
+            ):
+                raise ParentNotFoundError(
+                    "The app/namespace of the object to be created does not exist."
+                )
+            else:
+                raise exc  # pragma: no cover
+        return await self._get_single_object(type(obj), id=obj.id)  # type: ignore[attr-defined]
+
+    @error_guard
+    async def _update_object(self, orm_obj: ORMObj, /, **new_values) -> ORMObj:
+        async with self.session() as session:
+            for key, value in new_values.items():
+                setattr(orm_obj, key, value)
+            async with session.begin():
+                session.add(orm_obj)
+        return orm_obj
---
+++ management-api/tests/adapters/test_sql_persistence.py
@@ -0,0 +1,243 @@
+import pytest
+from guardian_management_api.adapters.sql_persistence import (
+    SQLAlchemyMixin,
+    error_guard,
+)
+from guardian_management_api.errors import (
+    ObjectExistsError,
+    ParentNotFoundError,
+    PersistenceError,
+)
+from guardian_management_api.models.sql_persistence import DBApp, DBNamespace
+from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
+
+
+@pytest.fixture
+def sqlalchemy_mixin(sqlite_url):
+    mixin = SQLAlchemyMixin()
+    mixin._db_string = SQLAlchemyMixin.create_db_string(
+        "sqlite", "", "", sqlite_url, "", ""
+    )
+    return mixin
+
+
+@pytest.mark.asyncio
+async def test_error_guard():
+    async def test_func():
+        raise SQLAlchemyError()
+
+    wrapped = error_guard(test_func)
+    with pytest.raises(PersistenceError, match="An unidentified error occurred."):
+        await wrapped()
+
+
+@pytest.mark.asyncio
+async def test_error_guard_uncaught_exception():
+    async def test_func():
+        raise ValueError("ORIGINAL_ERROR")
+
+    wrapped = error_guard(test_func)
+    with pytest.raises(ValueError, match="ORIGINAL_ERROR"):
+        await wrapped()
+
+
+class TestSQLAlchemyMixin:
+    def test_init(self):
+        obj = SQLAlchemyMixin()
+        assert obj._sql_engine is None
+        assert obj._session is None
+        assert obj._db_string == ""
+
+    @pytest.mark.parametrize(
+        "dialect,host,port,db_name,username,password,expected",
+        [
+            ("sqlite", "", "", "", "", "", "sqlite+aiosqlite://"),
+            ("sqlite", "a", "b", "c", "d", "e", "sqlite+aiosqlite:///c"),
+            ("postgresql", "a", "", "c", "d", "e", "postgresql+asyncpg://d:e@a/c"),
+            ("postgresql", "a", "b", "c", "d", "e", "postgresql+asyncpg://d:e@a:b/c"),
+            ("mysql", "a", "", "c", "d", "e", "mysql+aiomysql://d:e@a/c"),
+            ("mysql", "a", "b", "c", "d", "e", "mysql+aiomysql://d:e@a:b/c"),
+        ],
+    )
+    def test_create_db_string(
+        self, dialect, host, port, db_name, username, password, expected
+    ):
+        if dialect in ("mysql, postgresql"):
+            pytest.skip(
+                "Reactivate with univention/components/authorization-engine/guardian#98"
+            )
+        assert (
+            SQLAlchemyMixin.create_db_string(
+                dialect, host, port, db_name, username, password
+            )
+            == expected
+        )
+
+    def test_create_db_string_unsupported_dialect(self):
+        with pytest.raises(ValueError, match="The dialect FOO is not supported."):
+            SQLAlchemyMixin.create_db_string("FOO", "", "", "", "", "")
+
+    @pytest.mark.parametrize("dialect", ["mysql", "postgresql"])
+    @pytest.mark.parametrize(
+        "host,port,db_name,username,password",
+        [
+            ("", "", "", "", ""),
+            ("a", "", "", "", ""),
+            ("", "b", "", "", ""),
+            ("", "", "c", "", ""),
+            ("", "", "", "d", ""),
+            ("", "", "", "", "e"),
+            ("a", "b", "", "", ""),
+            ("a", "", "c", "", ""),
+            ("a", "", "", "d", ""),
+            ("a", "", "", "", "e"),
+            ("", "b", "c", "", ""),
+            ("", "b", "", "d", ""),
+            ("", "b", "", "", "e"),
+            ("", "", "c", "d", ""),
+            ("", "", "c", "", "e"),
+            ("", "", "", "d", "e"),
+            ("a", "b", "c", "", ""),
+            ("a", "b", "", "d", ""),
+            ("a", "b", "", "", "e"),
+            # Not all permutations tested, but this should be sufficient
+        ],
+    )
+    @pytest.mark.skip(
+        "Reactivate with univention/components/authorization-engine/guardian#98"
+    )
+    def test_create_db_string_missing_values(
+        self, dialect, host, port, db_name, username, password
+    ):
+        with pytest.raises(
+            ValueError,
+            match=f"The dialect {dialect} requires a host, db_name, username and password to connect.",
+        ):
+            SQLAlchemyMixin.create_db_string(
+                dialect, host, port, db_name, username, password
+            )
+
+    @pytest.mark.usefixtures("create_tables")
+    @pytest.mark.asyncio
+    async def test__get_single_object(self, sqlalchemy_mixin, create_app):
+        async with sqlalchemy_mixin.session() as session:
+            app = await create_app(session)
+        result = await sqlalchemy_mixin._get_single_object(DBApp, name="app")
+        assert result.name == app.name
+        assert result.display_name == app.display_name
+        assert result.id == app.id
+
+    @pytest.mark.usefixtures("create_tables")
+    @pytest.mark.asyncio
+    async def test__get_single_object_multiple_identifier(
+        self, sqlalchemy_mixin, create_app, create_namespace
+    ):
+        async with sqlalchemy_mixin.session() as session:
+            app = await create_app(session)
+            await session.commit()
+            namespace = await create_namespace(session, app_name=app.name)
+        result = await sqlalchemy_mixin._get_single_object(
+            DBNamespace, name="namespace", app_id=app.id
+        )
+        assert result.name == namespace.name
+        assert result.display_name == namespace.display_name
+        assert result.id == namespace.id
+        assert result.app_id == app.id
+
+    @pytest.mark.usefixtures("create_tables")
+    @pytest.mark.asyncio
+    async def test__get_single_object_none(
+        self, sqlalchemy_mixin, create_app, create_namespace
+    ):
+        async with sqlalchemy_mixin.session() as session:
+            app = await create_app(session)
+            await create_namespace(session, app_name=app.name)
+        result = await sqlalchemy_mixin._get_single_object(
+            DBNamespace, name="namespace2", app_id=app.id
+        )
+        assert result is None
+
+    @pytest.mark.parametrize("num_apps", [0, 1, 10, 100, 1056])
+    @pytest.mark.usefixtures("create_tables")
+    @pytest.mark.asyncio()
+    async def test__get_num_objects(self, sqlalchemy_mixin, create_apps, num_apps):
+        async with sqlalchemy_mixin.session() as session:
+            apps = await create_apps(session, num_apps)
+            counted_num = await sqlalchemy_mixin._get_num_objects(DBApp)
+        assert counted_num == len(apps)
+
+    @pytest.mark.asyncio
+    @pytest.mark.usefixtures("create_tables")
+    @pytest.mark.parametrize(
+        "limit,offset",
+        [
+            (5, 0),
+            (None, 0),
+            (5, 5),
+            (None, 20),
+            (1000, 0),
+            (1000, 5),
+            (None, 1000),
+            (5, 1000),
+            (0, 5),
+            (0, 0),
+        ],
+    )
+    async def test__get_many_objects(
+        self, sqlalchemy_mixin, create_namespaces, limit: int | None, offset
+    ):
+        async with sqlalchemy_mixin.session() as session:
+            namespaces = await create_namespaces(session, 10, 10)
+            namespaces.sort(key=lambda x: x.name)
+        selected_slice = (
+            namespaces[offset : offset + limit] if limit else namespaces[offset:]
+        )
+        result = await sqlalchemy_mixin._get_many_objects(DBNamespace, offset, limit)
+        assert [obj.name for obj in result] == [obj.name for obj in selected_slice]
+
+    @pytest.mark.asyncio
+    @pytest.mark.usefixtures("create_tables")
+    async def test__create_object(self, sqlalchemy_mixin, create_app):
+        async with sqlalchemy_mixin.session() as session:
+            app = await create_app(session)
+            ns = DBNamespace(app_id=app.id, name="ns", display_name="NS")
+            await sqlalchemy_mixin._create_object(ns)
+            async with session.begin():
+                found = (await session.scalars(select(DBNamespace))).one()
+                assert found.app_id == app.id
+                assert found.name == ns.name
+                assert found.display_name == ns.display_name
+
+    @pytest.mark.asyncio
+    @pytest.mark.usefixtures("create_tables")
+    async def test__create_object_exists_error(self, sqlalchemy_mixin, create_app):
+        async with sqlalchemy_mixin.session() as session:
+            await create_app(session, name="app")
+            with pytest.raises(
+                ObjectExistsError,
+                match="An object with the given identifiers already exists.",
+            ):
+                await sqlalchemy_mixin._create_object(
+                    DBApp(name="app", display_name="FOO")
+                )
+
+    @pytest.mark.asyncio
+    @pytest.mark.usefixtures("create_tables")
+    async def test__create_object_parent_not_found(self, sqlalchemy_mixin, create_app):
+        async with sqlalchemy_mixin.session() as session:
+            await create_app(session)
+            ns = DBNamespace(app_id=2, name="ns", display_name="NS")
+            with pytest.raises(
+                ParentNotFoundError,
+                match="The app/namespace of the object to be created does not exist.",
+            ):
+                await sqlalchemy_mixin._create_object(ns)
+
+    @pytest.mark.asyncio
+    @pytest.mark.usefixtures("create_tables")
+    async def test__update_object(self, sqlalchemy_mixin, create_app):
+        async with sqlalchemy_mixin.session() as session:
+            app = await create_app(session, display_name="App")
+        result = await sqlalchemy_mixin._update_object(app, display_name="FOO")
+        assert result.display_name == "FOO"
