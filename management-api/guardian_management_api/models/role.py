380512c8260264e5fc1c2e8da86eb47c65d0d9c6
Alexander Steffen <steffen@univention.de>
2023-09-13 07:27:19 +0200

    Issue #35:Implement routes and tests for roles

--- management-api/guardian_management_api/models/role.py
+++ management-api/guardian_management_api/models/role.py
@@ -3,17 +3,9 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 from dataclasses import dataclass
-from typing import Optional
+from typing import List, Optional
 
-from guardian_management_api.models.base import (
-    NamespacedResponseObject,
-    PaginationRequest,
-)
-
-
-class ResponseRole(NamespacedResponseObject):
-    app_name: str
-    name: str
+from .base import PaginatedAPIResponse, PaginationRequest, ResponseObject
 
 
 @dataclass(frozen=True)
@@ -22,6 +14,11 @@ class Role:
     namespace_name: str
     name: str
     display_name: Optional[str] = None
+
+
+@dataclass(frozen=True)
+class RoleCreateQuery:
+    roles: List[Role]
 
 
 @dataclass(frozen=True)
@@ -36,3 +33,21 @@ class RolesGetQuery:
     pagination: PaginationRequest
     app_name: Optional[str] = None
     namespace_name: Optional[str] = None
+
+
+@dataclass(frozen=True)
+class ResponseRole(ResponseObject):
+    app_name: str
+    namespace_name: str
+    name: str
+    display_name: Optional[str]
+
+
+@dataclass(frozen=True)
+class RoleAPIResponse:
+    role: ResponseRole
+
+
+@dataclass(frozen=True)
+class RolesListAPIResponse(PaginatedAPIResponse):
+    roles: List[ResponseRole]
