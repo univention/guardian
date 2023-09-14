3ff9127e0b5878ca80f8d642193cad8f8e8a5c7f
Alexander Steffen <steffen@univention.de>
2023-09-14 11:59:11 +0200

    Issue #22: Fixed setting password for user.

--- keycloak/provisioning/configure.sh
+++ keycloak/provisioning/configure.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-echo "Waiting 30 seconds for keycloak-guardian-dev to be reachable"
-sleep 30
-/opt/keycloak/bin/kcadm.sh config credentials --server http://traefik/guardian/keycloak --realm master --user admin --password admin
-sleep 1
-/opt/keycloak/bin/kcadm.sh create realms -s realm=guardian_realm -s enabled=true -o
-sleep 1
-/opt/keycloak/bin/kcadm.sh create roles -r guardian_realm -s name=user
-sleep 1
-/opt/keycloak/bin/kcadm.sh create clients -r guardian_realm -s clientId=guardian_client -s publicClient=true -o
-sleep 1
-/opt/keycloak/bin/kcadm.sh create users -r guardian_realm -s username=guardian_user -s enabled=true -o --fields id,username
-# sleep 1
-# /opt/keycloak/bin/kcadm.sh set-password -r guardian_realm --username guardian_user --password univention
+echo "Waiting for keycloak-guardian-dev to be reachable"
+while ! /opt/keycloak/bin/kcadm.sh config credentials --server http://traefik/guardian/keycloak --realm master --user admin --password admin; do sleep 5; done;
+
+#sleep 1
+/opt/keycloak/bin/kcadm.sh create realms -s realm=GuardianDev -s enabled=true -o
+#sleep 1
+/opt/keycloak/bin/kcadm.sh create roles -r GuardianDev -s name=user
+#sleep 1
+/opt/keycloak/bin/kcadm.sh create roles -r GuardianDev -s name=admin
+#sleep 1
+/opt/keycloak/bin/kcadm.sh create clients -r GuardianDev -s clientId=guardian_client -s publicClient=true -o
+#sleep 1
+/opt/keycloak/bin/kcadm.sh create groups -r GuardianDev -b '{"name": "Admins"}'
+#sleep 1
+/opt/keycloak/bin/kcadm.sh create users -r GuardianDev -s username=guardian_user -s enabled=true -o --fields id,username
+#sleep 1
+/opt/keycloak/bin/kcadm.sh set-password -r GuardianDev --username guardian_user --new-password univention
