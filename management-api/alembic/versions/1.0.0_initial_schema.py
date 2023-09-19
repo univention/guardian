0275a1d72fdcf916bbf44e55cedd71401909fea3
Ole Schwiegert <schwiegert@univention.de>
2023-09-19 11:33:30 +0200

    univention/components/authorization-engine/guardian#88: Add super user role including permissions and capability

--- management-api/alembic/versions/1.0.0_initial_schema.py
+++ management-api/alembic/versions/1.0.0_initial_schema.py
@@ -9,7 +9,7 @@ import base64
 import json
 import os.path
 from pathlib import Path
-from typing import Sequence, Union
+from typing import Sequence, Tuple, Union
 
 import sqlalchemy as sa
 from alembic import op
@@ -22,7 +22,7 @@ branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
-def create_guardian_app(session, app_table, ns_table, role_table) -> int:
+def create_guardian_app(session, app_table, ns_table, role_table) -> Tuple[int, int]:
     app_data = {"name": "guardian", "display_name": "The Guardian Application"}
     app_id = session.execute(insert(app_table).values(app_data)).lastrowid
     ns_default = {
@@ -43,7 +43,105 @@ def create_guardian_app(session, app_table, ns_table, role_table) -> int:
         "display_name": "The app admin for the Guardian Application",
     }
     session.execute(insert(role_table).values(role_data))
-    return ns_id
+    return app_id, ns_id
+
+
+def create_management_permissions(session, permission_table, ns_id):
+    permissions = {}
+    for perm_data in [
+        {
+            "namespace_id": ns_id,
+            "name": "create_resource",
+            "display_name": "Create Resource",
+        },
+        {
+            "namespace_id": ns_id,
+            "name": "read_resource",
+            "display_name": "Read Resource",
+        },
+        {
+            "namespace_id": ns_id,
+            "name": "update_resource",
+            "display_name": "Update Resource",
+        },
+        {
+            "namespace_id": ns_id,
+            "name": "delete_resource",
+            "display_name": "Delete Resource",
+        },
+    ]:
+        permissions[perm_data["name"]] = session.execute(
+            insert(permission_table).values(perm_data)
+        ).lastrowid
+    return permissions
+
+
+def create_super_admin(
+    session,
+    role_table,
+    cap_table,
+    cap_perm_table,
+    cap_cond_table,
+    builtin_ns,
+    management_ns,
+    permissions,
+    conditions,
+):
+    super_admin = {
+        "namespace_id": builtin_ns,
+        "name": "super-admin",
+        "display_name": "The super admin for the Guardian Application",
+    }
+    super_admin_id = session.execute(
+        insert(role_table).values(super_admin)
+    ).inserted_primary_key[0]
+    super_cap_data = {
+        "namespace_id": management_ns,
+        "name": "super_admin_cap",
+        "display_name": "The super admin capability",
+        "role_id": super_admin_id,
+        "relation": "OR",
+    }
+    cap_id = session.execute(
+        insert(cap_table).values(super_cap_data)
+    ).inserted_primary_key[0]
+    for perm_id in permissions.values():
+        session.execute(
+            insert(cap_perm_table).values(
+                {"capability_id": cap_id, "permission_id": perm_id}
+            )
+        )
+    for field, value in [("namespace_name", "builtin"), ("app_name", "guardian")]:
+        session.execute(
+            insert(cap_cond_table).values(
+                {
+                    "capability_id": cap_id,
+                    "condition_id": conditions["target_field_not_equals_value"],
+                    "kwargs": [
+                        {"name": "field", "value": field},
+                        {"name": "value", "value": value},
+                    ],
+                }
+            )
+        )
+    read_all_cap_data = {
+        "namespace_id": management_ns,
+        "name": "super_admin_cap_read_all",
+        "display_name": "The super admin capability to read everything",
+        "role_id": super_admin_id,
+        "relation": "AND",
+    }
+    read_all_cap_id = session.execute(
+        insert(cap_table).values(read_all_cap_data)
+    ).inserted_primary_key[0]
+    session.execute(
+        insert(cap_perm_table).values(
+            {
+                "capability_id": read_all_cap_id,
+                "permission_id": permissions["read_resource"],
+            }
+        )
+    )
 
 
 def create_builtin_conditions(session, cond_table, ns_id, cond_param_table):
@@ -122,7 +220,7 @@ def upgrade() -> None:
         sa.PrimaryKeyConstraint("id"),
         sa.UniqueConstraint("namespace_id", "name"),
     )
-    op.create_table(
+    permission_table = op.create_table(
         "permission",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("namespace_id", sa.Integer(), nullable=False),
@@ -148,7 +246,7 @@ def upgrade() -> None:
         sa.PrimaryKeyConstraint("id"),
         sa.UniqueConstraint("namespace_id", "name"),
     )
-    op.create_table(
+    cap_table = op.create_table(
         "capability",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("namespace_id", sa.Integer(), nullable=False),
@@ -208,7 +306,7 @@ def upgrade() -> None:
         sa.ForeignKeyConstraint(["condition_id"], ["condition.id"], ondelete="CASCADE"),
         sa.PrimaryKeyConstraint("id"),
     )
-    op.create_table(
+    cap_perm_table = op.create_table(
         "capability_permission",
         sa.Column("capability_id", sa.Integer(), nullable=False),
         sa.Column("permission_id", sa.Integer(), nullable=False),
@@ -225,8 +323,33 @@ def upgrade() -> None:
     # Setup Guardian default objects
     bind = op.get_bind()
     session = orm.Session(bind=bind)
-    ns_id = create_guardian_app(session, app_table, ns_table, role_table)
-    create_builtin_conditions(session, cond_table, ns_id, cond_param_table)
+    app_id, builtin_ns_id = create_guardian_app(
+        session, app_table, ns_table, role_table
+    )
+    management_ns_id = session.execute(
+        insert(ns_table).values(
+            {
+                "app_id": app_id,
+                "name": "management-api",
+                "display_name": "Management API Namespace of the Guardian Application",
+            }
+        )
+    ).lastrowid
+    create_builtin_conditions(session, cond_table, builtin_ns_id, cond_param_table)
+    permissions = create_management_permissions(
+        session, permission_table, management_ns_id
+    )
+    create_super_admin(
+        session,
+        role_table,
+        cap_table,
+        cap_perm_table,
+        cap_cond_table,
+        builtin_ns_id,
+        management_ns_id,
+        permissions,
+        conditions,
+    )
 
 
 def downgrade() -> None:
