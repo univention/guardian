145c2b7b26217047c75810d88b5602c4b1721576
Alexander Steffen <steffen@univention.de>
2023-11-07 11:36:54 +0100

    Issue #28: Cleanup comments

--- appcenter-management/files/guardian_syntax.py
+++ appcenter-management/files/guardian_syntax.py
@@ -15,7 +15,6 @@ class GuardianRole(simple):
     regex = re.compile(
         r"^([a-z0-9-_]+:[a-z0-9-_]+:[a-z0-9-_]+)(&[a-z0-9-_]+:[a-z0-9-_]+:[a-z0-9-_]+)?$"
     )
-    # TODO prod: Add translation: univention/components/authorization-engine/guardian#28
     error_message = _(
         "Guardian role strings must be lowercase ASCII alphanumeric with hyphens and underscores, "
         "in the format 'app:namespace:role' or 'app:namespace:role&app:namespace:context'!"
