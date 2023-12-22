35ec77d2143fcc06ba872cb15b38ca409ad8eb45
cgarcia <garcia-maurino.extern@univention.de>
2023-12-22 11:41:42 +0100

    Issue #207: M2M secret file instead of setting

---
+++ management-api/entrypoint.d/m2m_secret.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -f /guardian_service_dir/conf/m2m.secret ]; then
+    echo "Copying m2m secret file..."
+    mkdir mkdir -p /guardian_service_dir/conf
+    cp /var/lib/univention-appcenter/apps/guardian-management-api/conf/m2m.secret /guardian_service_dir/conf/m2m.secret
+    chmod 600 /guardian_service_dir/conf/m2m.secret
+    chown guardian: /guardian_service_dir/conf/m2m.secret
+fi
