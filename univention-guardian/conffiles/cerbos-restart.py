47e16dd757b8ac920cb8903a6576adad183dd1d0
Christian Castens <castens@univention.de>
2026-06-15 15:41:25 +0200

    feat: add ucr variable for toggling cerbos audit logging
    univention/dev/projects/authorization-engine/guardian#287

---
+++ univention-guardian/conffiles/cerbos-restart.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2026 Univention GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
+
+from subprocess import call  # nosec B404
+
+
+def postinst(ucr, changes):
+    call(
+        ["/usr/bin/systemctl", "try-restart", "univention-guardian-server"]
+    )  # nosec B603
